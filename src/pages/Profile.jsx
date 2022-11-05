7f68b7e6b08b6d7d742f96e25ad7492f86e8fc74
Gabriela Muniz <gabrielanamu@live.com>
2022-11-05 18:41:20 -0300

    adiciona requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loding from './Loding';
 
 class Profile extends Component {
+  state = {
+    loading: false,
+    userInfo: '',
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    const user = await getUser();
+    this.setState({
+      userInfo: user,
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading, userInfo } = this.state;
+    // if (loading === true) {
+    //   return <Loding />;
+    // }
     return (
       <div data-testid="page-profile">
         <Header />
+        {loading && <Loding />}
+        { userInfo !== ''
+        && (
+          <div>
+            <img
+              src={ userInfo.image }
+              alt={ userInfo.name }
+              data-testid="profile-image"
+            />
+            <Link to="/profile/edit">Editar perfil</Link>
+            <h3>{userInfo.name}</h3>
+            <p>{userInfo.email}</p>
+            <p>{userInfo.description}</p>
+          </div>
+        )}
       </div>
     );
   }
